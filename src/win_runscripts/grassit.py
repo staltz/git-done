59ccbc237007bf6b9fb136bcf6ab240e229afa38
STALTZ <staltz@benaia>
2012-09-20 17:18:28 +0300

    Added run scripts

---
+++ src/win_runscripts/grassit.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+import sys
+import os.path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir)))
+
+import grassitcore
+
+if __name__ == '__main__':
+    grassitcore.main()
+
