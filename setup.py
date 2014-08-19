41d079b828a9e39d9e70de174dc6cbc4002a615c
Andre Medeiros <andre.medeiros@futurice.com>
2014-08-19 17:02:59 +0300

    setup.py refers to README.md

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
 	url='https://github.com/staltz/git-done',
 	license='LICENSE.txt',
 	description="A git extension to manage todos and commit messages.",
-	long_description=open('README.txt').read(),
+	long_description=open('README.md').read(),
 	install_requires=[
 		"GitPython == 0.3.2.RC1",
 		"termcolor == 1.1.0",
